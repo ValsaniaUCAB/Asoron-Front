e0c4c0ca07bb63083b2bdbfa8f8c0fc4704b9535
Alessandro <alessvalsania@gmail.com>
2023-12-03 20:29:53 -0400

    Mejorando Catalogo

--- src/modules/catalogo/components/Ron.vue
+++ src/modules/catalogo/components/Ron.vue
@@ -1,8 +1,11 @@
 <template>
-    <div>
+    <div class="box">
         <router-link :to="{ name: 'ron-detail', params: { id: ron.ron_id } }"><img
-                :src="ron.images[1].img_url" /></router-link>
-        <h3>{{ ron.ron_nombre }}</h3>
+                :src="ron.images[0].img_url" /></router-link>
+        <div class="nombre">
+            {{ ron.ron_nombre }}
+            <div class="precio d-flex justify-content-between"><span>Desde</span><span>$289</span></div>
+        </div>
 
     </div>
 </template>
@@ -26,11 +29,41 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.box {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 500px;
+    width: 370px;
+    padding: 30px 40px;
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
+    border-radius: 8px;
+    transition: transform 0.5s;
+
+    &:hover {
+        box-shadow: 0 0 20px rgba(253, 208, 141, 0.9);
+        transform: scale(1.05);
+    }
+}
+
+.nombre {
+    font-size: 16px;
+    font-weight: bold;
+    width: 100%;
+    text-align: left;
+}
+
+.precio {
+    color: #75869C;
+    font-size: 14px;
+    font-weight: normal;
+    width: 100%;
+    text-align: left;
+}
+
 img {
     height: 300px;
     width: 300px;
-    border: 1px solid lightgrey;
-    border-radius: 15px;
 }
 </style>
--- src/modules/catalogo/pages/Catalogo.vue
+++ src/modules/catalogo/pages/Catalogo.vue
@@ -1,16 +1,24 @@
 <template>
     <div>Soy Catalogo</div>
     <div>
-        <div class="mx-5">Productos
-            <div class="px-2 pt-2">
-                <input type="text" placeholder="Buscar Entrada" v-model="term">
+        <div class="header">
+            <div class="titulo">Productos</div>
+            <div class="barras">
+                <div class="busqueda">
+                    <div class="titulo-barras">Buscar</div>
+                    <input class="barra-busqueda" type="text" placeholder="Buscar Entrada" v-model="term">
+                </div>
+                <div class="sort">
+                    <div class="titulo-barras">Ordenar</div>
+                    <input class="barra-busqueda" type="text" placeholder="Buscar Entrada" v-model="term">
+                </div>
             </div>
         </div>
         <div class="d-flex">
             <div class="col-2">
                 Filtros
             </div>
-            <div class="d-flex">
+            <div class="catalogo">
                 <li v-for="ron in ronesByTerm">
                     <Ron :ron="ron" />
                 </li>
@@ -49,12 +57,71 @@ export default {
 }
 </script>
 
-<style scoped>
-.prod {
-    text-align: left
+<style lang="scss" scoped>
+.header {
+    display: flex;
+    flex-direction: column;
+    margin: 35px 0;
+}
+
+.catalogo {
+    display: flex;
+    flex-wrap: wrap;
+}
+
+.titulo {
+    font-family: 'Brothers', sans-serif;
+    font-size: 48px;
+    font-weight: bold;
+    color: #31212B;
+    width: 100%;
+    text-align: left;
+    margin: 30px;
+}
+
+.barras {
+
+    margin: 30px;
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    justify-content: right;
+
+}
+
+.busqueda {
+    margin-right: 10px;
+    display: flex;
+    flex-direction: column;
+    width: 470px;
+}
+
+.sort {
+    margin-right: 50px;
+    display: flex;
+    flex-direction: column;
+    width: 225px;
+}
+
+.titulo-barras {
+    color: #31212B;
+    width: 100%;
+    text-align: left;
+    font-size: 24px;
+    font-weight: bold;
+}
+
+.barra-busqueda {
+    height: 56px;
+    border: 2px solid #31212B;
+    border-radius: 16px;
 }
 
 li {
     list-style-type: none;
+}
+
+input {
+    text-indent: 20px;
 }
 </style>
