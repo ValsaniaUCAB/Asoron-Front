72f4b491b64dabd7bb2240c4e93c1433ba1441b4
Alessandro <alessvalsania@gmail.com>
2023-12-17 17:37:22 -0400

    correccion de errores

--- src/modules/profile/pages/PerfilClienteJuridico.vue
+++ src/modules/profile/pages/PerfilClienteJuridico.vue
@@ -1,10 +1,14 @@
 <template>
     <h1>Soy Perfil del Cliente Juridico</h1>
+    <Logout></Logout>
 </template>
 
 <script>
+import Logout from '../components/Logout'
 export default {
-
+    components: {
+        Logout
+    },
 }
 </script>
 
