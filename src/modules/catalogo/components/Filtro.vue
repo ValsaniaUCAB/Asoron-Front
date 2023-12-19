c96325d07e682a4bf4dfbfebb1e0ec65e5e5e445
GustavoFF7 <gafreitas.19@est.ucab.edu.ve>
2023-12-19 15:37:34 -0400

    se agregaron botones de acpetar y cancel al filtro

--- src/modules/catalogo/components/Filtro.vue
+++ src/modules/catalogo/components/Filtro.vue
@@ -1,6 +1,16 @@
 <template>
     <div class="filtros">
-        <h1>Filtros</h1>
+        <div class="title-container">
+            <h1>Filtros</h1>
+            <button class="title-btn apply-btn">
+                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="35" height="35" viewBox="0 0 50 50">
+                    <path d="M 41.9375 8.625 C 41.273438 8.648438 40.664063 9 40.3125 9.5625 L 21.5 38.34375 L 9.3125 27.8125 C 8.789063 27.269531 8.003906 27.066406 7.28125 27.292969 C 6.5625 27.515625 6.027344 28.125 5.902344 28.867188 C 5.777344 29.613281 6.078125 30.363281 6.6875 30.8125 L 20.625 42.875 C 21.0625 43.246094 21.640625 43.410156 22.207031 43.328125 C 22.777344 43.242188 23.28125 42.917969 23.59375 42.4375 L 43.6875 11.75 C 44.117188 11.121094 44.152344 10.308594 43.78125 9.644531 C 43.410156 8.984375 42.695313 8.589844 41.9375 8.625 Z"></path>
+                </svg>
+            </button>
+            <button class="title-btn cancel-btn">
+                <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="none" viewBox="0 0 50 50" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" stroke="currentColor"><path d="M20 4 4 20M4 4l16 16"/></svg>
+            </button>
+        </div>
         <div class="filtro-container filtro-marca">
             <h2 class="filtro-titulo">Marca : {{ this.marca }}</h2>
             <ul class="filtro-lista">
@@ -79,6 +89,28 @@ export default {
 </script>
 
 <style>
+
+.title-container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    padding-bottom: 30px;
+}
+
+.title-btn {
+    padding: 0;
+    background: none;
+    border: none;
+}
+
+.apply-btn svg {
+    fill: #FDD08D;
+}
+.cancel-btn svg {
+    fill: #31212b;
+}
+
 .filtros {
     padding-left: 44px;
     padding-top: 52px;
@@ -92,7 +124,6 @@ export default {
 
 .filtros h1 {
     font-size: 36px;
-    padding-bottom: 30px;
     font-weight: 600;
 }
 
