363e2ce066d411c26296ec2b3473ecbdbe422d0a
GustavoFF7 <gafreitas.19@est.ucab.edu.ve>
2023-12-27 01:46:57 -0400

    se corrigio about us de asoron

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -7,33 +7,34 @@
                 <path d="M101.2 101H69.8399L71.1199 90.7599L69.1999 83.0799L67.9199 81.7999L61.5199 84.9999H53.1999L46.7999 81.7999L45.5199 83.0799L43.5999 90.7599L44.8799 101H15.4399L20.5599 90.7599L38.4799 19.7199L36.5599 11.3999H80.0799L78.1599 19.7199L96.0799 90.7599L101.2 101ZM64.0799 60.0399L57.9359 33.7999L57.2959 29.3199L56.6559 33.7999L50.5119 60.0399L51.5359 62.5999L54.0959 63.8799H60.4959L63.0559 62.5999L64.0799 60.0399ZM167.105 84.3599L150.465 101H115.265L106.305 92.0399L102.465 84.3599V79.2399L122.945 68.9999V76.0399L126.785 79.8799H135.745L139.585 77.9599L141.505 74.1199V68.3599L138.305 65.1599L118.465 60.6799L106.945 49.1599V28.0399L123.585 11.3999H158.145L166.209 35.7199L155.201 31.8799H138.305L134.465 33.7999L132.545 37.6399V40.1999L135.105 42.7599L154.945 47.2399L167.105 59.3999V84.3599ZM254.14 84.3599L237.5 101H197.82L181.18 84.3599V28.0399L197.82 11.3999H237.5L254.14 28.0399V84.3599ZM228.54 72.1999V38.9199L225.98 33.7999L220.86 31.2399H214.46L209.34 33.7999L206.78 38.9199V72.1999L209.34 77.3199L214.46 79.8799H220.86L225.98 77.3199L228.54 72.1999ZM346.995 82.4399L337.395 101.64H323.955L311.155 88.8399V72.1999L309.875 70.9199H298.995L297.715 72.1999V90.7599L300.915 101H270.195L273.395 90.7599V21.6399L270.195 11.3999H325.875L342.515 28.0399V51.7199L331.635 62.5999L334.835 65.7999V75.3999L339.315 79.8799L346.995 82.4399ZM317.555 42.7599V39.5599L314.995 34.4399L309.875 31.8799H298.995L297.715 33.1599V49.1599L298.995 50.4399H309.875L314.995 47.8799L317.555 42.7599ZM430.89 84.3599L414.25 101H374.57L357.93 84.3599V28.0399L374.57 11.3999H414.25L430.89 28.0399V84.3599ZM405.29 72.1999V38.9199L402.73 33.7999L397.61 31.2399H391.21L386.09 33.7999L383.53 38.9199V72.1999L386.09 77.3199L391.21 79.8799H397.61L402.73 77.3199L405.29 72.1999ZM522.465 101H495.585L498.145 90.7599V88.1999L473.825 51.7199L471.265 45.3199L471.905 51.7199V90.7599L475.105 101H446.945L450.145 90.7599V21.6399L446.305 11.3999H473.825L496.225 45.0639L498.785 51.0799L498.145 44.9359V21.6399L494.945 11.3999H522.465L519.265 21.6399V90.7599L522.465 101Z" fill="#FFA217"/>
                 <path d="M86.1999 89.9999H54.8399L56.1199 79.7599L54.1999 72.0799L52.9199 70.7999L46.5199 73.9999H38.1999L31.7999 70.7999L30.5199 72.0799L28.5999 79.7599L29.8799 89.9999H0.439941L5.55994 79.7599L23.4799 8.7199L21.5599 0.399902H65.0799L63.1599 8.7199L81.0799 79.7599L86.1999 89.9999ZM49.0799 49.0399L42.9359 22.7999L42.2959 18.3199L41.6559 22.7999L35.5119 49.0399L36.5359 51.5999L39.0959 52.8799H45.4959L48.0559 51.5999L49.0799 49.0399ZM152.105 73.3599L135.465 89.9999H100.265L91.3049 81.0399L87.4649 73.3599V68.2399L107.945 57.9999V65.0399L111.785 68.8799H120.745L124.585 66.9599L126.505 63.1199V57.3599L123.305 54.1599L103.465 49.6799L91.9449 38.1599V17.0399L108.585 0.399902H143.145L151.209 24.7199L140.201 20.8799H123.305L119.465 22.7999L117.545 26.6399V29.1999L120.105 31.7599L139.945 36.2399L152.105 48.3999V73.3599ZM239.14 73.3599L222.5 89.9999H182.82L166.18 73.3599V17.0399L182.82 0.399902H222.5L239.14 17.0399V73.3599ZM213.54 61.1999V27.9199L210.98 22.7999L205.86 20.2399H199.46L194.34 22.7999L191.78 27.9199V61.1999L194.34 66.3199L199.46 68.8799H205.86L210.98 66.3199L213.54 61.1999ZM331.995 71.4399L322.395 90.6399H308.955L296.155 77.8399V61.1999L294.875 59.9199H283.995L282.715 61.1999V79.7599L285.915 89.9999H255.195L258.395 79.7599V10.6399L255.195 0.399902H310.875L327.515 17.0399V40.7199L316.635 51.5999L319.835 54.7999V64.3999L324.315 68.8799L331.995 71.4399ZM302.555 31.7599V28.5599L299.995 23.4399L294.875 20.8799H283.995L282.715 22.1599V38.1599L283.995 39.4399H294.875L299.995 36.8799L302.555 31.7599ZM415.89 73.3599L399.25 89.9999H359.57L342.93 73.3599V17.0399L359.57 0.399902H399.25L415.89 17.0399V73.3599ZM390.29 61.1999V27.9199L387.73 22.7999L382.61 20.2399H376.21L371.09 22.7999L368.53 27.9199V61.1999L371.09 66.3199L376.21 68.8799H382.61L387.73 66.3199L390.29 61.1999ZM507.465 89.9999H480.585L483.145 79.7599V77.1999L458.825 40.7199L456.265 34.3199L456.905 40.7199V79.7599L460.105 89.9999H431.945L435.145 79.7599V10.6399L431.305 0.399902H458.825L481.225 34.0639L483.785 40.0799L483.145 33.9359V10.6399L479.945 0.399902H507.465L504.265 10.6399V79.7599L507.465 89.9999Z" fill="#FDD08D"/>
             </svg>
-            <p>Lorem ipsum dolor sit amet consectetur. Felis pellentesque est tellus felis ultrices fermentum aenean. 
-                Mi sagittis habitant sociis volutpat in. Diam consequat in faucibus sed sem viverra scelerisque proin tristique. 
-                Facilisis neque molestie posuere duis diam id sed habitant.</p>
+            <p>Somos la única organización mayorista que opera en el mercado venezolano, vendiendo
+                grandes volúmenes de ron a clientes profesionales registrados. Hoteles, restaurantes,
+                comedores, bodegas, empresas prestadoras de servicios y demás comercios afiliados a
+                ASORONUCAB se benefician por la compra de productos al mayor. Así como también lo
+                hacen los venezolanos para abastecerse al más bajo costo.</p>
         </div>
         <div class="mision-container">
             <img class="img-filler" src="@/assets/about_mision.jpg">
             <div class="info">
                 <h1>MISIÓN</h1>
-                <p>Lorem ipsum dolor sit amet consectetur. Nec eu adipiscing mauris nunc in pellentesque varius laoreet. 
-                    Integer senectus sem auctor faucibus. Dictumst nunc urna posuere vestibulum cursus in. Nulla et feugiat 
-                    iaculis praesent amet nunc velit lectus ridiculus. Id ultricies mi leo vulputate ante eu. Dignissim amet 
-                    mi elit dictum at mattis faucibus. Eget facilisis molestie elementum consectetur etiam dictum. Vivamus 
-                    vulputate placerat diam nulla elementum suspendisse erat. Dolor sed imperdiet sit consectetur commodo 
-                    vitae. Morbi at quam massa consectetur nullam. Dictum tellus id morbi sapien nunc consequat.
-                    Arcu leo libero etiam habitant pharetra semper tellus sed. Sed condimentum nec ultricies sed accumsan nulla mattis mattis. </p>
+                <p>La misión de ASORONUCAB es la distribución de ron venezolano con excelencia en precio,
+                calidad y variedad para clientes que compran volumen, ofreciéndole ventajas y
+                oportunidades para crecer. Para nuestros clientes, el objetivo es ser la fuente más segura
+                de productos, dándoles la oportunidad de competir en sus respectivos mercados. Para
+                nuestros miembros ser los distribuidores de sus productos al más bajo costo posible en el
+                máximo número de puntos de venta. </p>
             </div>
         </div>
         <div class="whyron-container">
             <div class="info">
                 <h1>¿POR QUE RON?</h1>
-                <p>Lorem ipsum dolor sit amet consectetur. Nec eu adipiscing mauris nunc in pellentesque varius laoreet. 
-                    Integer senectus sem auctor faucibus. Dictumst nunc urna posuere vestibulum cursus in. Nulla et feugiat 
-                    iaculis praesent amet nunc velit lectus ridiculus. Id ultricies mi leo vulputate ante eu. Dignissim amet 
-                    mi elit dictum at mattis faucibus. Eget facilisis molestie elementum consectetur etiam dictum. Vivamus 
-                    vulputate placerat diam nulla elementum suspendisse erat. Dolor sed imperdiet sit consectetur commodo 
-                    vitae. Morbi at quam massa consectetur nullam. Dictum tellus id morbi sapien nunc consequat.
-                    Arcu leo libero etiam habitant pharetra semper tellus sed. Sed condimentum nec ultricies sed accumsan nulla mattis mattis. </p>
+                <p> Hemos elegido vender ron como parte de nuestra estrategia centrada en el cliente, buscando proporcionar
+                     productos de calidad y servicios que se alineen estrechamente con las necesidades y expectativas 
+                     de nuestros clientes en el mercado mayorista venezolano. Esta estrategia centrada en el cliente no solo 
+                     se trata de vender ron; es sobre construir una asociación sólida y duradera con nuestros clientes. Entendemos que el éxito a largo plazo 
+                     proviene de comprender y adaptarse constantemente a las cambiantes preferencias y demandas del mercado. 
+                     Nosotros no solo vendemos ron, vende una experiencia única y 
+                     satisfactoria que refleja nuestra dedicación a la excelencia y al servicio al cliente.</p>
             </div>
             <img class="img-filler" src="@/assets/about_whyron.jpg">
         </div>
@@ -90,7 +91,6 @@ export default {
     .mision-container {
         display: flex;
         flex-direction: row;
-        align-items: center;
         justify-content: center;
         margin-top: 100px;
     }
@@ -99,7 +99,7 @@ export default {
         display: flex;
         flex-direction: column;
         align-items: flex-start;
-        justify-content: left;
+        justify-content: flex-start;
         margin-left: 50px;
         margin-right: 50px;
         color: black;
@@ -113,7 +113,7 @@ export default {
     }
 
     .info p {
-        width: 564px;
+        width: 500px;
         text-align: left;
     }
 
